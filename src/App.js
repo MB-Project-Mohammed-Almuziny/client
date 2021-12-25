91785a2fb52c320652ff2f2bce949f6e08a4f467
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-12-25 21:56:12 +0300

    create register components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router";
 import { Box } from "@mui/material";
 
 import { Header } from "./components/Header";
+import { Register } from "./pages/Register";
 import { Login } from "./pages/Login";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<> home </>} />
+        <Route path="/register" element={<Register />} />
         <Route path="/LogIn" element={<Login />} />
       </Routes>
     </Box>
---
+++ src/pages/Register.js
@@ -0,0 +1,111 @@
+import { React, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import axios from "axios";
+import {
+  Container,
+  Box,
+  FormGroup,
+  TextField,
+  Typography,
+  Button,
+} from "@mui/material";
+
+export const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (
+      password.match(
+        // eslint-disable-next-line
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[!@#\$%\^&\*]).{8,}$/
+      )
+    ) {
+      axios
+        .post(`${process.env.REACT_APP_BASE_URL}/user/register`, {
+          name,
+          email,
+          password,
+        })
+        .then(() => {
+          navigate("/logIn");
+
+          Swal.fire({
+            position: "top",
+            icon: "success",
+            title: "verification link sent to your email",
+          });
+        })
+        .catch((err) => {
+          if (err.response.data.error.split(" ")[0] === "E11000")
+            Swal.fire({
+              position: "top",
+              icon: "warning",
+              text: "this email already exceeded",
+            });
+          else console.log(err);
+        });
+    } else {
+      Swal.fire({
+        position: "top",
+        icon: "warning",
+        title: "your password is weak",
+        text: "the password have top be at least 6 character and contain at least 1 Capital letter, small, special character, and number ",
+      });
+    }
+  };
+
+  return (
+    <Container maxWidth="md">
+      <Typography variant="h3" align="center" mb={2}>
+        register
+      </Typography>
+      <Box sx={{ bgcolor: "background.paper", p: 2 }}>
+        <form onSubmit={handleSubmit}>
+          <FormGroup>
+            <TextField
+              onChange={(e) => setName(e.target.value)}
+              fullWidth
+              id="userName"
+              label="User Name"
+              placeholder="User Name"
+              margin="normal"
+              required
+            />
+            <TextField
+              onChange={(e) => setEmail(e.target.value)}
+              fullWidth
+              type="email"
+              id="email"
+              label="email"
+              placeholder="Email"
+              margin="normal"
+              required
+            />
+            <TextField
+              onChange={(e) => setPassword(e.target.value)}
+              fullWidth
+              type="password"
+              id="password"
+              label="password"
+              placeholder="password"
+              margin="normal"
+              required
+            />
+          </FormGroup>
+          <Typography align="center" my={2}>
+            <Button variant="contained" type="submit">
+              register
+            </Button>
+          </Typography>
+        </form>
+      </Box>
+    </Container>
+  );
+};
