37f3e8e5597e8c1e223e8dbaed2064d4c312a0e3
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-12-25 23:22:14 +0300

    create home components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router";
 import { Box } from "@mui/material";
 
 import { Header } from "./components/Header";
+import { Home } from "./pages/Home";
 import { Register } from "./pages/Register";
 import { Login } from "./pages/Login";
 
@@ -11,7 +12,7 @@ function App() {
       <Header />
 
       <Routes>
-        <Route path="/" element={<> home </>} />
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/LogIn" element={<Login />} />
       </Routes>
