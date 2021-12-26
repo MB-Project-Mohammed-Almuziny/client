5ed3c2b81ee5055c07620bef4f4aaa458d8d8022
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-12-26 15:39:28 +0300

    create course info componrnts

--- src/pages/CourseInfo.js
+++ src/pages/CourseInfo.js
@@ -1,14 +1,20 @@
 import { React, useState, useEffect } from "react";
 import { useParams } from "react-router";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import axios from "axios";
+import Swal from "sweetalert2";
 import ReactStars from "react-rating-stars-component";
-import { Container, Typography } from "@mui/material";
+import { Container, Typography, Button } from "@mui/material";
 import CircularProgress from "@mui/material/CircularProgress";
 
 export const CourseInfo = () => {
   const [course, setCourse] = useState();
 
+  const { userId } = useSelector((state) => state.account);
   const { courseId } = useParams();
+
+  const navigate = useNavigate();
 
   const getCourseInfo = () => {
     try {
@@ -25,23 +31,54 @@ export const CourseInfo = () => {
     }
   };
 
+  const handleEnrole = (e) => {
+    e.preventDefault();
+
+    try {
+      axios
+        .post(`${process.env.REACT_APP_BASE_URL}/user/enrole`, {
+          userId,
+          courseId,
+        })
+        .then((result) => {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Your login successfully",
+            showConfirmButton: false,
+            timer: 1000,
+          });
+
+          navigate("/");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   useEffect(() => {
     getCourseInfo();
   }, []);
 
-  useEffect(() => {
-    console.log(course);
-  }, [course]);
-
   return course ? (
     <Container>
+      <Button onClick={handleEnrole} variant="contained">
+        Enrole now
+      </Button>
+
       <Typography>{course.title}</Typography>
       <Typography>created by: {course.creator.name}</Typography>
       <hr />
+
       <Typography variant="h6">Description</Typography>
       <Typography>{course.description}</Typography>
+
       <Typography variant="h6">About</Typography>
       <Typography>{course.about}</Typography>
+
       <Typography variant="h6">feedback</Typography>
       <ReactStars
         count={5}
