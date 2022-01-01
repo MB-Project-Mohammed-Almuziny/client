f129978adad5cc2adeaf437c74d4ca4808b23a4d
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2022-01-01 23:47:26 +0300

    update course card component

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -7,19 +7,13 @@ const { Link } = Typography;
 export const CourseCard = ({ course }) => {
   const navigate = useNavigate();
 
-  const handleClick = () => {
-    navigate("/course/" + course._id);
-  };
-
-  const handleUserInfo = (e) => {
-    e.preventDefault();
-
-    navigate("/user/" + course.creator._id);
-  };
-
   return (
     <Col span={6}>
-      <Card hoverable cover={<img alt="example" src={course.thumbnail} />}>
+      <Card
+        hoverable
+        cover={<img alt="example" src={course.thumbnail} />}
+        onClick={() => navigate("/course/" + course._id)}
+      >
         <Card.Meta
           title={course.title}
           description={
