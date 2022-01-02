e3037546f55173d8f650328173b9de83061c8bd2
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2022-01-03 00:11:42 +0300

    update course by term page

--- src/pages/CoursesByTerm.js
+++ src/pages/CoursesByTerm.js
@@ -1,8 +1,7 @@
 import { React, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import { Container, Typography } from "@mui/material";
-import { Row } from "antd";
+import { Layout, Row } from "antd";
 
 import { CourseCard } from "../components/CourseCard";
 
@@ -32,16 +31,14 @@ export const CoursesByTerm = () => {
   }, [term]);
 
   return (
-    <Container>
-      <Typography variant="h3" align="center" my={2}>
-        courses with {term} term
-      </Typography>
+    <Layout.Content className="content">
+      <h1 className="title">courses with {term} term</h1>
 
       <Row gutter={[24, 24]}>
         {courses.map((course) => (
           <CourseCard course={course} key={course._id} />
         ))}
       </Row>
-    </Container>
+    </Layout.Content>
   );
 };
